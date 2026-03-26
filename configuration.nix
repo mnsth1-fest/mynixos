96ea1179f8cec0f2dfc3a6c68f5f30e2d0527d0a
mnsth1-fest <monsterz.zxc@gmail.com>
2026-03-26 22:11:05 +0200

    Add files via upload

--- configuration.nix
+++ configuration.nix
@@ -28,7 +28,9 @@
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
+ 
+  programs.nix-ld.enable = true;
+ 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
   # console = {
@@ -38,26 +40,37 @@
   # };
 
   # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-
+  #services.xserver.enable = true;
+  services.flatpak.enable = true;
+  #programs.flatpak.enable = true;
+  #xdg.portal.enable = true;
   #Nvidia
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.graphics.enable = true;
   hardware.graphics.enable32Bit = true;   
   hardware.nvidia.open = false;
   hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.latest;
-
+  
   #Hyprland  
-  programs.hyprland = {
+  #programs.hyprland = {
+  #  enable = true;
+  #  withUWSM = true;
+  #  xwayland.enable = true;
+  #};
+
+  # Enable Plasma 
+  services.desktopManager.plasma6.enable = true;
+
+  # Default display manager for Plasma
+  services.displayManager.sddm = {
     enable = true;
-    withUWSM = true;
-    xwayland.enable = true;
+  
+  # To use Wayland (Experimental for SDDM)
+    wayland.enable = true;
   };
 
-  environment.sessionVariables = {
-    XCURSOR_THEME = "Adwaita";
-    XCURSOR_SIZE = "24";
-  };
+  # Optionally enable xserver
+  services.xserver.enable = true;
 
   programs.zsh = {
   enable = true;
@@ -67,11 +80,11 @@
   histSize = 10000;
   };
   # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+  services.xserver.xkb.layout = "us";
+  services.xserver.xkb.options = "eurosign:e,caps:escape";
 
   # Enable CUPS to print documents.
-  # services.printing.enable = true;
+  services.printing.enable = true;
 
   # Enable sound.
   # services.pulseaudio.enable = true;
@@ -90,6 +103,8 @@
     users.mofest = {
       isNormalUser = true;
       extraGroups = [ "wheel" "networkmanager" ];
+      packages = with pkgs; [
+      ];
     };
   };
 
@@ -103,77 +118,41 @@
    environment.systemPackages = with pkgs; [
      fastfetch
      htop
-     discord
-     kitty
+     #kitty
      kdePackages.dolphin
      steam
-     adwaita-icon-theme
-     waybar
-     matugen
-     hyprlock
+     #waybar
+     #matugen
+     obsidian
+     #hyprlock
      cava
      cmatrix
      pipes
      git
      pavucontrol
      zsh-powerlevel10k
-     grim
-     slurp
-     wl-clipboard
+     #grim
+     #slurp
+     #wl-clipboard
      jetbrains.idea-oss
      jetbrains.pycharm-oss
-     obsidian
      wofi
+     #swaybg
+     python3
+     obs-studio
+     krita
+     mpv
    ];
 
    fonts.packages = with pkgs; [
     nerd-fonts.jetbrains-mono
     noto-fonts
+    fira-code
+    fira-code-symbols
     noto-fonts-color-emoji
     font-awesome
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "25.11"; # Did you read the comment?
 
 }
